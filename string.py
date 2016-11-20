47b8f6d55aecc9dc590cfbadb8c2063d28ef13e8
Prakhar <srivprakhar@gmail.com>
2016-11-20 02:00:07 +0100

    More Examples

--- string.py
+++ string.py
@@ -1,1 +1,22 @@
-print 123
+#!/usr/bin/python
+""" Learning python
+"""
+NAME = "PrakharSrivastav"
+
+# String functions
+print len(NAME)
+print NAME.find('a')
+print NAME.capitalize()
+print NAME.upper()
+print NAME.lower()
+print NAME.center(30, "*")
+print NAME.isalpha()
+print NAME.swapcase()
+print "    ".isspace()
+print NAME.rjust(24, '-')
+print NAME.partition('S')
+print NAME.split('a')
+
+#String Index
+print NAME[: -5]
+
